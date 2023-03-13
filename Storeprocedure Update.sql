a4da65b9d5a3212035c8288016329ce394be78dd
sabbirshikder <mdsabbirshikder1218@gmail.com>
2023-03-13 09:55:58 +0600

    Initial Commit

---
+++ Storeprocedure Update.sql
@@ -0,0 +1,15 @@
+SELECT name, create_date, modify_date 
+FROM sys.objects
+WHERE type = 'p'
+ORDER BY modify_date DESC
+
+-- p means procedure 
+-- u means table
+
+SELECT Name
+FROM sys.procedures
+WHERE OBJECT_DEFINITION(OBJECT_ID) LIKE '%MonthlyPaymentOfferData%'
+-- Find Table Name Where used this table in store procedure 
+
+-- Find The column Name All Tables 
+ Select * from INFORMATION_SCHEMA.COLUMNS Where COLUMN_NAME like '%Vehicle%'
